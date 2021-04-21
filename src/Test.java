6ff2c9785775f47e41c9b209b72b7e147eead65e
zpz24 <35415265+zpz24@users.noreply.github.com>
2021-04-20 22:01:42 -0700

    first test.java commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Test {
+	public static void main (String[] args) {
+		System.out.println("adfsdfs");
+		ArrayList<String> list = new ArrayList<>();
+		list.add("abc");
+		list.add("123");
+	}
+}
